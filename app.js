10aa3aaa930b4210c35b9721ec621aa5c1244ef2
Dano-19 <dam.cardenas@yavirac.edu.ec>
2025-10-23 12:26:18 -0500

    commit_4

--- app.js
+++ app.js
@@ -11,3 +11,10 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+
+if (require.main === module) {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+  });
+}
