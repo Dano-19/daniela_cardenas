3a74deca12fefa97c67a4451fab95ced4bbf0847
Tefy89 <eds.ordonez@yavirac.edu.ec>
2025-10-23 11:43:18 -0500

    first commit

---
+++ tests/app.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Pruebas del servidor', () => {
+  test('Ruta principal responde con Hola Mundo', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toContain('Hola Mundo');
+  });
+
+  test('Ruta inexistente responde con 404', async () => {
+    const res = await request(app).get('/noexiste');
+    expect(res.statusCode).toBe(404);
+  });
+});
