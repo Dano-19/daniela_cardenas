805cc9f9625e0f489b96585b12adc650ac774e7d
Dano-19 <dam.cardenas@yavirac.edu.ec>
2025-10-23 12:19:44 -0500

    Commit_corregido

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola, aqui esta elerror intencional');
+  res.send('Hola Mundo');
 });
 
 app.listen(PORT, () => {
